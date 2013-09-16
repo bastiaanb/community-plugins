fa384ee60afc836150e60f2c3777cd5b93dc421e
Bastiaan Bakker <bbakker@xebia.com>
2013-09-16 17:34:19 +0200

    ensure the output file will be closed always. don't catch expections when loading the CI: there could be other reasons than a wrong CI name for a load to fail.

--- cli-scripts/composite-application-descriptor-exporter/export-composite-application-descriptor.py
+++ cli-scripts/composite-application-descriptor-exporter/export-composite-application-descriptor.py
@@ -1,24 +1,21 @@
 import string
 
 def exportCompositePackage(compositeName, targetFile):
-  try:
-    compositePackage = repository.read('Applications/' + compositeName)
-  except:
-    print "ERROR: Unknown Composite Package '%s'. Please verify that an item with ID 'Applications/%s' exists in the repository" % (compositeName, compositeName)
-    return
+  compositePackage = repository.read('Applications/' + compositeName)
 
   f = open(targetFile, 'w')
-  [app, ver] = string.rsplit(compositePackage.id, "/", 1)
-  f.write("application: %s\n" % (app))
-  f.write("version: %s\n\n" % (ver))
+  try:
+    [app, ver] = string.rsplit(compositePackage.id, "/", 1)
+    f.write("application: %s\n" % (app))
+    f.write("version: %s\n\n" % (ver))
 
-  i = 1
-  for package in compositePackage.packages:
-    [app, ver] = string.rsplit(package, "/", 1)
-    f.write("package.%s.name=%s\n" % (i, app))
-    f.write("package.%s.version=%s\n\n" % (i, ver))
-    i = i + 1
+    i = 1
+    for package in compositePackage.packages:
+      [app, ver] = string.rsplit(package, "/", 1)
+      f.write("package.%s.name=%s\n" % (i, app))
+      f.write("package.%s.version=%s\n\n" % (i, ver))
+      i = i + 1
+  finally:
+    f.close()
 
-  f.close()
   return
-
