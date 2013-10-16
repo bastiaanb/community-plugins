3015257da3a5f30e104b81b73974f5d8ec8314a7
Bastiaan Bakker <bbakker@xebia.com>
2013-10-16 23:42:59 +0200

    skip all checking if Enforcement level is NONE. refactored variable names.

--- deployit-udm-plugins/utility-plugins/deployable-mapping-verifier-plugin/src/main/java/com/xebialabs/deployit/community/verifier/DeploymentMappingVerifier.java
+++ deployit-udm-plugins/utility-plugins/deployable-mapping-verifier-plugin/src/main/java/com/xebialabs/deployit/community/verifier/DeploymentMappingVerifier.java
@@ -10,33 +10,37 @@ import com.xebialabs.deployit.plugin.api.udm.Deployable;
 import com.xebialabs.deployit.plugin.api.udm.Deployed;
 import com.xebialabs.deployit.plugin.api.udm.DeployedApplication;
 
+import static com.xebialabs.deployit.community.verifier.RequiredInstancesEnforcement.NONE;
+
 public class DeploymentMappingVerifier {
     public static final String REQUIRED_INSTANCES_PER_ENVIRONMENT_PROPERTY = "requiredInstancesPerEnvironment";
     public static final String REQUIRED_INSTANCES_ENFORCEMENT_PROPERTY = "requiredInstancesEnforcement";
 
     public List<String> validate(DeployedApplication deployedApplication) {
-        final RequiredInstancesEnforcement mustBeMappedEnforcementLevel =
-            deployedApplication.getEnvironment().getProperty(REQUIRED_INSTANCES_ENFORCEMENT_PROPERTY);
-        final Set<Deployable> allDeployables = deployedApplication.getVersion().getDeployables();
-        final Map<Deployable, Integer> deployedCountsPerDeployable = new HashMap<Deployable, Integer>(allDeployables.size());
-        for (final Deployable deployabe : allDeployables) {
-            deployedCountsPerDeployable.put(deployabe, 0);
-        }
-
-        for (final Deployed<?, ?> deployed : deployedApplication.getDeployeds()) {
-            final Deployable deployable = deployed.getDeployable();
-            deployedCountsPerDeployable.put(deployable, deployedCountsPerDeployable.get(deployable) + 1);
-        }
-
         final List<String> errorMessages = new ArrayList<String>();
+        final RequiredInstancesEnforcement requiredInstancesEnforcement =
+            deployedApplication.getEnvironment().getProperty(REQUIRED_INSTANCES_ENFORCEMENT_PROPERTY);
 
-        for (Map.Entry<Deployable, Integer> entry : deployedCountsPerDeployable.entrySet()) {
-            final Deployable deployable = entry.getKey();
-            RequiredInstancesPerEnvironment mustBeMapped = deployable.getProperty(REQUIRED_INSTANCES_PER_ENVIRONMENT_PROPERTY);
+        if (!requiredInstancesEnforcement.equals(NONE)) {
+            final Set<Deployable> allDeployables = deployedApplication.getVersion().getDeployables();
+            final Map<Deployable, Integer> deployedCountsPerDeployable = new HashMap<Deployable, Integer>(allDeployables.size());
+            for (final Deployable deployabe : allDeployables) {
+                deployedCountsPerDeployable.put(deployabe, 0);
+            }
 
-            if (!mustBeMapped.isCompliant(entry.getValue(), mustBeMappedEnforcementLevel)) {
-                errorMessages.add(String.format("deployable %s would have %d instances, but required is %s",
-                    deployable.getId(), entry.getValue(), mustBeMapped));
+            for (final Deployed<?, ?> deployed : deployedApplication.getDeployeds()) {
+                final Deployable deployable = deployed.getDeployable();
+                deployedCountsPerDeployable.put(deployable, deployedCountsPerDeployable.get(deployable) + 1);
+            }
+
+            for (Map.Entry<Deployable, Integer> entry : deployedCountsPerDeployable.entrySet()) {
+                final Deployable deployable = entry.getKey();
+                RequiredInstancesPerEnvironment requiredInstancesPerEnvironment = deployable.getProperty(REQUIRED_INSTANCES_PER_ENVIRONMENT_PROPERTY);
+
+                if (!requiredInstancesPerEnvironment.isCompliant(entry.getValue(), requiredInstancesEnforcement)) {
+                    errorMessages.add(String.format("Deployable %s has been mapped %d times, but required is %s",
+                        deployable.getId(), entry.getValue(), requiredInstancesPerEnvironment));
+                }
             }
         }
 
--- deployit-udm-plugins/utility-plugins/deployable-mapping-verifier-plugin/src/test/java/com/xebialabs/deployit/community/verifier/DeploymentMappingVerifierTest.java
+++ deployit-udm-plugins/utility-plugins/deployable-mapping-verifier-plugin/src/test/java/com/xebialabs/deployit/community/verifier/DeploymentMappingVerifierTest.java
@@ -22,138 +22,133 @@ import static org.mockito.Mockito.when;
 
 public class DeploymentMappingVerifierTest {
 
-	@Mock
-    private DeployedApplication deployedApplication;
-	
-	@Mock
-	private Environment environment;
+    @Mock private DeployedApplication deployedApplication;
 
-	@SuppressWarnings("rawtypes")
-	private Deployed deployed1;
+    @Mock private Environment environment;
 
-	@SuppressWarnings("rawtypes")
-	private Deployed deployed2;
+    @SuppressWarnings("rawtypes") private Deployed deployed1;
 
-	@SuppressWarnings("rawtypes")
-	private Deployed deployed2b;
+    @SuppressWarnings("rawtypes") private Deployed deployed2;
 
-	private Deployable deployable1;
-	
-	private Deployable deployable2;
-	
-	private DeploymentMappingVerifier verifier;
-	
-	@Before
+    @SuppressWarnings("rawtypes") private Deployed deployed2b;
+
+    private Deployable deployable1;
+
+    private Deployable deployable2;
+
+    private DeploymentMappingVerifier verifier;
+
+    @Before
     public void initMocks() throws Exception {
         MockitoAnnotations.initMocks(this);
 
         deployable1 = mockDeployable("Applications/completeDeploymentTest/1.0/test-command-1");
         deployable2 = mockDeployable("Applications/completeDeploymentTest/1.0/test-command-2");
-        
+
         final Version version = mock(Version.class);
-        when(version.getId()).thenReturn("Applications/completeDeploymentTest/1.0");        
+        when(version.getId()).thenReturn("Applications/completeDeploymentTest/1.0");
         when(version.getDeployables()).thenReturn(new HashSet<Deployable>(Arrays.asList(deployable1, deployable2)));
 
         when(deployedApplication.getVersion()).thenReturn(version);
         when(deployedApplication.getEnvironment()).thenReturn(environment);
-        
+
         deployed1 = mockDeployed("Infrastructure/test-server-1/test-command-1", deployable1);
         deployed2 = mockDeployed("Infrastructure/test-server-1/test-command-2", deployable2);
         deployed2b = mockDeployed("Infrastructure/test-server-2/test-command-2", deployable2);
-        
-		verifier = new DeploymentMappingVerifier();		
+
+        verifier = new DeploymentMappingVerifier();
     }
-    
-	@SuppressWarnings("rawtypes")
-	@Test
-	public void completeDeploymentShouldPassValidation() {
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void completeDeploymentShouldPassValidation() {
         setRequiredInstancesPerEnvironment(deployable1, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesPerEnvironment(deployable2, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesEnforcement(environment, RequiredInstancesEnforcement.FULL);
-		when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2)));
+        when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2)));
 
         List<String> errorMessages = verifier.validate(deployedApplication);
 
         assertTrue(errorMessages.isEmpty());
-	}
+    }
 
     @SuppressWarnings("rawtypes")
-	@Test
-	public void incompleteDeploymentShouldFailValidation() {
+    @Test
+    public void incompleteDeploymentShouldFailValidation() {
         setRequiredInstancesPerEnvironment(deployable1, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesPerEnvironment(deployable2, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesEnforcement(environment, RequiredInstancesEnforcement.FULL);
-		when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1)));
+        when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1)));
 
         List<String> errorMessages = verifier.validate(deployedApplication);
 
         assertFalse(errorMessages.isEmpty());
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Test
-	public void incompleteDeploymentShouldPassValidationIfDeployableCardinalityIsOptional() {
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void incompleteDeploymentShouldPassValidationIfDeployableCardinalityIsOptional() {
         setRequiredInstancesPerEnvironment(deployable1, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesPerEnvironment(deployable2, RequiredInstancesPerEnvironment.ZERO_OR_MORE);
         setRequiredInstancesEnforcement(environment, RequiredInstancesEnforcement.FULL);
-		when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1)));
+        when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1)));
 
         List<String> errorMessages = verifier.validate(deployedApplication);
 
         assertTrue(errorMessages.isEmpty());
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Test
-	public void redundantDeploymentShouldPassValidationIfDeployableCardinalityIsRedudant() {
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void redundantDeploymentShouldPassValidationIfDeployableCardinalityIsRedudant() {
         setRequiredInstancesPerEnvironment(deployable1, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesPerEnvironment(deployable2, RequiredInstancesPerEnvironment.TWO_OR_MORE);
         setRequiredInstancesEnforcement(environment, RequiredInstancesEnforcement.FULL);
-		when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2, deployed2b)));
+        when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2, deployed2b)));
 
         List<String> errorMessages = verifier.validate(deployedApplication);
 
         assertTrue(errorMessages.isEmpty());
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Test
-	public void nonRedundantDeploymentShouldFailValidationIfDeployableCardinalityIsRedudant() {
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void nonRedundantDeploymentShouldFailValidationIfDeployableCardinalityIsRedudant() {
         setRequiredInstancesPerEnvironment(deployable1, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesPerEnvironment(deployable2, RequiredInstancesPerEnvironment.TWO_OR_MORE);
         setRequiredInstancesEnforcement(environment, RequiredInstancesEnforcement.FULL);
-		when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2)));
+        when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2)));
 
         List<String> errorMessages = verifier.validate(deployedApplication);
 
         assertFalse(errorMessages.isEmpty());
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Test
-	public void nonRedundantDeploymentShouldPassValidationIfDeployableCardinalityIsRedudantAndEnvironmentIgnoresRedundancyRequirements() {
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void nonRedundantDeploymentShouldPassValidationIfDeployableCardinalityIsRedudantAndEnvironmentIgnoresRedundancyRequirements() {
         setRequiredInstancesPerEnvironment(deployable1, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesPerEnvironment(deployable2, RequiredInstancesPerEnvironment.TWO_OR_MORE);
         setRequiredInstancesEnforcement(environment, RequiredInstancesEnforcement.LENIENT);
-		when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2)));
+        when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1, deployed2)));
 
         List<String> errorMessages = verifier.validate(deployedApplication);
 
         assertTrue(errorMessages.isEmpty());
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Test
-	public void incompleteDeploymentShouldPassValidationIfEnvironmentIgnoresCardinalityRequirements() {
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void incompleteDeploymentShouldPassValidationIfEnvironmentIgnoresCardinalityRequirements() {
         setRequiredInstancesPerEnvironment(deployable1, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesPerEnvironment(deployable2, RequiredInstancesPerEnvironment.ONE_OR_MORE);
         setRequiredInstancesEnforcement(environment, RequiredInstancesEnforcement.NONE);
-		when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1)));
+        when(deployedApplication.getDeployeds()).thenReturn(new HashSet<Deployed>(Arrays.asList(deployed1)));
 
         List<String> errorMessages = verifier.validate(deployedApplication);
 
         assertTrue(errorMessages.isEmpty());
-	}
+    }
 
     private void setRequiredInstancesEnforcement(Environment environment, RequiredInstancesEnforcement enforcementLevel) {
         when(environment.hasProperty("requiredInstancesEnforcement")).thenReturn(true);
@@ -165,17 +160,17 @@ public class DeploymentMappingVerifierTest {
         when(deployable.getProperty("requiredInstancesPerEnvironment")).thenReturn(mustBeMapped);
     }
 
-	private Deployable mockDeployable(final String id) {
+    private Deployable mockDeployable(final String id) {
         Deployable deployable = mock(Deployable.class);
         when(deployable.getId()).thenReturn(id);
         return deployable;
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	private Deployed mockDeployed(final String id, final Deployable deployable) {
-		Deployed deployed = mock(Deployed.class);
-		when(deployed.getId()).thenReturn(id);
-		when(deployed.getDeployable()).thenReturn(deployable);
-		return deployed;
-	}
+    @SuppressWarnings("rawtypes")
+    private Deployed mockDeployed(final String id, final Deployable deployable) {
+        Deployed deployed = mock(Deployed.class);
+        when(deployed.getId()).thenReturn(id);
+        when(deployed.getDeployable()).thenReturn(deployable);
+        return deployed;
+    }
 }
